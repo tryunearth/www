47be52b5221dcfee1e5da78f341f49f02d7ce5d2
Cedricium <amaya.cedric@gmail.com>
2020-06-05 22:58:39 -0700

    Revert "Add simple 404 Not Found page"
    This reverts commit 8f6f84ed3c2fc9515b662d1560559081bc60b188.
    
    For some reason (bugs are so much fun), the Navbar for main section
    of the 404 page is dark, mimicking that of the Home page. It should
    instead be light—need to investigate further.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import { Router } from '@reach/router'
 
-import { Home, About, NotFound } from './pages'
+import { Home, About } from './pages'
 import './App.css'
 
 const App = () => (
   <Router>
-    <NotFound default />
     <Home exact path='/' />
     <About path='/about' />
   </Router>
